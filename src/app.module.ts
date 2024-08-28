04c1315233bbf4e6900048778a6ce8e50a338ca0
otiodarth <otiodarth@gmail.com>
2024-08-28 03:43:42 -0300

    feat: add event management init resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { EventManagementModule } from './module/event-management/event.management.module';
-import { AppConfigModule } from './module/shared/config/app-config.module';
+import { EventManagementModule } from '@eventManagement/event.management.module';
+import { AppConfigModule } from '@shared/config/app-config.module';
 
 @Module({
   imports: [AppConfigModule, EventManagementModule],
